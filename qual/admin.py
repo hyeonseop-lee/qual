ffd4b907056b29946cf30b51f1bcfb5b153cdc8f
Lee Hyeonseop <protos37@gmail.com>
2015-07-19 00:24:17 +0900

    Converted tabs to spaces in .py files

--- qual/admin.py
+++ qual/admin.py
@@ -6,57 +6,57 @@ from qual import admin, db
 from qual.models import User, Problem, Category, ProblemSet, ProblemSetScore
 
 class AdminView(BaseView):
-	def is_accessible(self):
-		return current_user.is_authenticated() and current_user.admin
+    def is_accessible(self):
+        return current_user.is_authenticated() and current_user.admin
 
 class UserView(AdminView, ModelView):
-	can_create = False
-	column_list = ('realname', 'username', 'nickname', 'score', 'admin')
-	form_columns = ('realname', 'username', 'nickname', 'admin')
+    can_create = False
+    column_list = ('realname', 'username', 'nickname', 'score', 'admin')
+    form_columns = ('realname', 'username', 'nickname', 'admin')
 
-	def on_model_change(self, form, user, is_create):
-		ProblemSetScore.query.filter_by(user_id=user.id).update({'nickname': user.nickname})
+    def on_model_change(self, form, user, is_create):
+        ProblemSetScore.query.filter_by(user_id=user.id).update({'nickname': user.nickname})
 
-	def on_mode_delete(self, user):
-		ProblemSetScore.query.filter_by(user_id=user.id).delete()
+    def on_mode_delete(self, user):
+        ProblemSetScore.query.filter_by(user_id=user.id).delete()
 
 class ProblemView(AdminView, ModelView):
-	column_list = ('title', 'category_name', 'score')
-	form_columns = ('title', 'content', 'flag', 'score', 'category')
+    column_list = ('title', 'category_name', 'score')
+    form_columns = ('title', 'content', 'flag', 'score', 'category')
 
-	def on_model_change(self, form, problem, is_create):
-		problem.category_name = problem.category.name
-		for solver in problem.solvers:
-			solver.build_score()
-		for problemset in problem.problemsets:
-			problemset.build_score()
-		for category in Category.query.all():
-			category.problems_count = len(category.problems.all())
+    def on_model_change(self, form, problem, is_create):
+        problem.category_name = problem.category.name
+        for solver in problem.solvers:
+            solver.build_score()
+        for problemset in problem.problemsets:
+            problemset.build_score()
+        for category in Category.query.all():
+            category.problems_count = len(category.problems.all())
 
-	def on_model_delete(self, problem):
-		for solver in problem.solvers:
-			solver.solves.remove(problem)
-			solver.build_score()
-		for problemset in problem.problemsets:
-			problemset.problems.remove(problem)
-			problemset.problems_count -= 1
-			problemset.build_score()
-		problem.category.problems_count -= 1
+    def on_model_delete(self, problem):
+        for solver in problem.solvers:
+            solver.solves.remove(problem)
+            solver.build_score()
+        for problemset in problem.problemsets:
+            problemset.problems.remove(problem)
+            problemset.problems_count -= 1
+            problemset.build_score()
+        problem.category.problems_count -= 1
 
 class CategoryView(AdminView, ModelView):
-	column_list = ('name', )
-	form_columns = ('name', )
+    column_list = ('name', )
+    form_columns = ('name', )
 
 class ProblemSetView(AdminView, ModelView):
-	column_list = ('title', )
-	form_columns = ('title', 'problems')
+    column_list = ('title', )
+    form_columns = ('title', 'problems')
 
-	def on_model_change(self, form, problemset, is_create):
-		problemset.problems_count = len(problemset.problems.all())
-		problemset.build_score()
+    def on_model_change(self, form, problemset, is_create):
+        problemset.problems_count = len(problemset.problems.all())
+        problemset.build_score()
 
-	def on_model_delete(self, problemset):
-		ProblemSetScore.query.filter_by(problemset_id=problemset.id).delete()
+    def on_model_delete(self, problemset):
+        ProblemSetScore.query.filter_by(problemset_id=problemset.id).delete()
 
 class RankView(AdminView):
     @expose('/')
--- qual/models.py
+++ qual/models.py
@@ -3,141 +3,141 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from qual import db
 
 solves = db.Table('solves',
-		db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-		db.Column('problem_id', db.Integer, db.ForeignKey('problem.id'))
+        db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+        db.Column('problem_id', db.Integer, db.ForeignKey('problem.id'))
 )
 
 includes = db.Table('includes',
-		db.Column('problemset_id', db.Integer, db.ForeignKey('problemset.id')),
-		db.Column('problem_id', db.Integer, db.ForeignKey('problem.id'))
+        db.Column('problemset_id', db.Integer, db.ForeignKey('problemset.id')),
+        db.Column('problem_id', db.Integer, db.ForeignKey('problem.id'))
 )
 
 class User(db.Model):
-	__tablename__ = 'user'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
-	nickname = db.Column(db.String(64), index=True, unique=True)
-        realname = db.Column(db.String(64))
-	password = db.Column(db.String(256))
-	score = db.Column(db.Integer)
-	admin = db.Column(db.Boolean)
-	solves = db.relationship('Problem', secondary=solves, backref=db.backref('solvers', lazy='dynamic'), lazy='dynamic')
-	updated_at = db.Column(db.DateTime)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    realname = db.Column(db.String(64))
+    password = db.Column(db.String(256))
+    score = db.Column(db.Integer)
+    admin = db.Column(db.Boolean)
+    solves = db.relationship('Problem', secondary=solves, backref=db.backref('solvers', lazy='dynamic'), lazy='dynamic')
+    updated_at = db.Column(db.DateTime)
 
-	def __init__(self, username, nickname, password, admin=False):
-		self.username = username
-		self.nickname = nickname
-		self.set_password(password)
-		self.score = 0
-		self.admin = admin
-		self.updated_at = datetime.datetime.utcnow()
+    def __init__(self, username, nickname, password, admin=False):
+        self.username = username
+        self.nickname = nickname
+        self.set_password(password)
+        self.score = 0
+        self.admin = admin
+        self.updated_at = datetime.datetime.utcnow()
 
-	def __repr__(self):
-		return '<User %s %s>' % (self.username, self.nickname)
+    def __repr__(self):
+        return '<User %s %s>' % (self.username, self.nickname)
 
-	def set_password(self, password):
-		self.password = generate_password_hash(password)
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-	def check_password(self, password):
-		return check_password_hash(self.password, password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-	def is_authenticated(self):
-		return True
+    def is_authenticated(self):
+        return True
 
-	def is_active(self):
-		return True
+    def is_active(self):
+        return True
 
-	def is_anonymous(self):
-		return False
+    def is_anonymous(self):
+        return False
 
-	def get_id(self):
-		return unicode(self.id)
+    def get_id(self):
+        return unicode(self.id)
 
-	def solved(self, problem):
-		return db.session.query(solves).filter_by(user_id=self.id, problem_id=problem.id).first()
+    def solved(self, problem):
+        return db.session.query(solves).filter_by(user_id=self.id, problem_id=problem.id).first()
 
-	def solve(self, problem):
-		self.updated_at = datetime.datetime.utcnow()
-		self.score += problem.score
-		self.solves.append(problem)
-		for problemset in problem.problemsets:
-			score = ProblemSetScore.query.filter_by(problemset_id=problemset.id, user_id=self.id).first()
-			if not score:
-				score = ProblemSetScore(problemset, self)
-				db.session.add(score)
-			score.score += problem.score
-			score.updated_at = datetime.datetime.utcnow()
+    def solve(self, problem):
+        self.updated_at = datetime.datetime.utcnow()
+        self.score += problem.score
+        self.solves.append(problem)
+        for problemset in problem.problemsets:
+            score = ProblemSetScore.query.filter_by(problemset_id=problemset.id, user_id=self.id).first()
+            if not score:
+                score = ProblemSetScore(problemset, self)
+                db.session.add(score)
+            score.score += problem.score
+            score.updated_at = datetime.datetime.utcnow()
 
-	def build_score(self):
-		self.score = 0
-		for problem in self.solves:
-			self.score += problem.score
+    def build_score(self):
+        self.score = 0
+        for problem in self.solves:
+            self.score += problem.score
 
 class Problem(db.Model):
-	__tablename__ = 'problem'
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(64), index=True, unique=True)
-	content = db.Column(db.UnicodeText)
-	flag = db.Column(db.String(256))
-	score = db.Column(db.Integer)
-	category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-	category_name = db.Column(db.String(64))
+    __tablename__ = 'problem'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=True, unique=True)
+    content = db.Column(db.UnicodeText)
+    flag = db.Column(db.String(256))
+    score = db.Column(db.Integer)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category_name = db.Column(db.String(64))
 
-	def __repr__(self):
-		return '<Problem %s%d %s>' % (self.category_name, self.score, self.title)
+    def __repr__(self):
+        return '<Problem %s%d %s>' % (self.category_name, self.score, self.title)
 
-	def check_flag(self, flag):
-		return self.flag == flag
+    def check_flag(self, flag):
+        return self.flag == flag
 
 class Category(db.Model):
-	__tablename__ = 'category'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), index=True, unique=True)
-	problems = db.relationship('Problem', backref='category', lazy='dynamic')
-	problems_count = db.Column(db.Integer)
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    problems = db.relationship('Problem', backref='category', lazy='dynamic')
+    problems_count = db.Column(db.Integer)
 
-	def __init__(self, name=''):
-		self.name = name
-		self.problems_count = 0
+    def __init__(self, name=''):
+        self.name = name
+        self.problems_count = 0
 
-	def __repr__(self):
-		return '<Category %s>' % (self.name)
+    def __repr__(self):
+        return '<Category %s>' % (self.name)
 
 class ProblemSet(db.Model):
-	__tablename__ = 'problemset'
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(64), index=True, unique=True)
-	problems = db.relationship('Problem', secondary=includes, backref=db.backref('problemsets', lazy='dynamic'), lazy='dynamic')
-	problems_count = db.Column(db.Integer)
+    __tablename__ = 'problemset'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=True, unique=True)
+    problems = db.relationship('Problem', secondary=includes, backref=db.backref('problemsets', lazy='dynamic'), lazy='dynamic')
+    problems_count = db.Column(db.Integer)
 
-	def __repr__(self):
-		return '<ProblemSet %s>' % (self.title)
+    def __repr__(self):
+        return '<ProblemSet %s>' % (self.title)
 
-	def build_score(self):
-		for score in ProblemSetScore.query.filter_by(problemset_id=self.id).all():
-			db.session.delete(score)
-		total = {}
-		for problem in self.problems:
-			for solver in problem.solvers:
-				if not solver in total:
-					total[solver] = 0
-				total[solver] += problem.score
-		for solver in total:
-			score = ProblemSetScore(self, solver, total[solver])
-			db.session.add(score)
+    def build_score(self):
+        for score in ProblemSetScore.query.filter_by(problemset_id=self.id).all():
+            db.session.delete(score)
+        total = {}
+        for problem in self.problems:
+            for solver in problem.solvers:
+                if not solver in total:
+                    total[solver] = 0
+                total[solver] += problem.score
+        for solver in total:
+            score = ProblemSetScore(self, solver, total[solver])
+            db.session.add(score)
 
 class ProblemSetScore(db.Model):
-	__tablename__ = 'problemsetscore'
-	id = db.Column(db.Integer, primary_key=True)
-	problemset_id = db.Column(db.Integer, db.ForeignKey('problemset.id'))
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-	nickname = db.Column(db.String(64))
-	score = db.Column(db.Integer)
-	updated_at = db.Column(db.DateTime)
+    __tablename__ = 'problemsetscore'
+    id = db.Column(db.Integer, primary_key=True)
+    problemset_id = db.Column(db.Integer, db.ForeignKey('problemset.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    nickname = db.Column(db.String(64))
+    score = db.Column(db.Integer)
+    updated_at = db.Column(db.DateTime)
 
-	def __init__(self, problemset, user, score=0):
-		self.problemset_id = problemset.id
-		self.user_id = user.id
-		self.nickname = user.nickname
-		self.score = score
-		self.updated_at = datetime.datetime.utcnow()
+    def __init__(self, problemset, user, score=0):
+        self.problemset_id = problemset.id
+        self.user_id = user.id
+        self.nickname = user.nickname
+        self.score = score
+        self.updated_at = datetime.datetime.utcnow()
